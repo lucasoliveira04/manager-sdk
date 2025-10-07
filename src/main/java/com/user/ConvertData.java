9396d15b0240ac3d4781b025564ec7a0f1141665
Lucas <lucasolisocialmedia@gmail.com>
2025-10-07 16:35:33 -0300

    init

---
+++ src/main/java/com/user/ConvertData.java
@@ -0,0 +1,19 @@
+package com.user;
+
+public class ConvertData {
+    public String convert(int num) {
+        return Integer.toString(num);
+    }
+
+    public String convert(long num) {
+        return Long.toString(num);
+    }
+
+    public String convert(double num) {
+        return Double.toString(num);
+    }
+
+    public String convert(float num) {
+        return Float.toString(num);
+    }
+}
