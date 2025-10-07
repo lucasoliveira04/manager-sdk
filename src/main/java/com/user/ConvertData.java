1ee36ddd223bebc194b378771c2d506725da5cb6
Lucas <lucasolisocialmedia@gmail.com>
2025-10-07 20:59:47 -0300

    Versão 1.0.0-SNAPSHOT

--- src/main/java/com/user/ConvertData.java
+++ src/main/java/com/user/ConvertData.java
@@ -5,8 +5,4 @@ public class ConvertData {
     public String convertIntToString(int value){
         return Integer.toString(value);
     }
-
-    public String convertIntToString(int value,int min,int max){
-        return Integer.toString(value);
-    }
 }
