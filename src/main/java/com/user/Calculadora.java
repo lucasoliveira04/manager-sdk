0fcd16414e8b75f1d961e01c8e28251bde7bf1d5
Lucas <lucasolisocialmedia@gmail.com>
2025-10-07 17:51:24 -0300

    Versão 1.0.0

--- src/main/java/com/user/Calculadora.java
+++
@@ -1,8 +0,0 @@
-package com.user;
-
-public class Calculadora {
-
-    public int soma(int a, int b) {
-        return a + b;
-    }
-}
--- src/main/java/com/user/Descript.java
+++
@@ -1,8 +0,0 @@
-package com.user;
-
-public class Descript {
-
-    public String descript(int num) {
-        return Integer.toString(num);
-    }
-}
